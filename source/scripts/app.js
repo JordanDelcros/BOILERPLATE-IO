c16bfc7cc74d4c70aea13e53dc6d1591a7570a0c
Jordan Delcros <contact@jordan-delcros.com>
2017-01-06 14:50:27 +0100

    Created three boilerplate

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -1,3 +1,7 @@
+import * as THREE from "three";
+
+const DEGREE = (Math.PI / 180);
+
 class App {
 	constructor(){
 
@@ -6,15 +10,88 @@ class App {
 	}
 	initialize(){
 
-		var message = "Hello boilerplate world!";
+		this.renderer = new THREE.WebGLRenderer({
+			precision: "highp",
+			antialias: true
+		});
 
-		var paragraph = document.querySelector("p");
+		this.renderer.shadowMap.enabled = true;
+		this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-		paragraph.textContent = message;
+		document.body.appendChild(this.renderer.domElement);
 
-		console.log(message);
+		this.scene = new THREE.Scene();
+
+		this.camera = new THREE.PerspectiveCamera(45, 1, 0.1, 1000);
+		this.camera.position.set(0, 0, 10);
+
+		var texture = new THREE.TextureLoader().load("/assets/images/default.jpg");
+
+		this.cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1, 1), new THREE.MeshPhongMaterial({
+			map: texture
+		}));
+		this.cube.castShadow = true;
+		this.scene.add(this.cube);
+
+		this.plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10, 1, 1), new THREE.MeshPhongMaterial());
+		this.plane.rotation.x = -(90 * DEGREE);
+		this.plane.position.set(0, -2, 0);
+		this.plane.receiveShadow = true;
+		this.scene.add(this.plane);
+
+		this.light = new THREE.PointLight(0xFFFFFF, 1, 100);
+		this.light.position.set(0, 10, 10);
+		this.light.castShadow = true;
+		this.light.shadowMapWidth = this.light.shadowMapHeight = 2048;
+		this.scene.add(this.light);
+
+		window.addEventListener("resize", this.resize.bind(this), false);
+
+		this.resize();
+		this.start();
 
 		return this;
+
+	}
+	resize(){
+
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+		this.ratio = (this.width / this.height);
+
+		this.camera.aspect = this.ratio;
+		this.camera.updateProjectionMatrix();
+
+		this.renderer.setSize(this.width, this.height);
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+
+		return this;
+
+	}
+	start(){
+
+		this.animationFrame = window.requestAnimationFrame(this.update.bind(this));
+
+	}
+	stop(){
+
+		window.cancelAnimationFrame(this.animationFrame);
+
+	}
+	update( now ){
+
+		this.animationFrame = window.requestAnimationFrame(this.update.bind(this));
+
+		this.cube.rotation.x += (DEGREE * 1);
+		this.cube.rotation.y += (DEGREE * 1);
+		this.cube.rotation.z += (DEGREE * 1);
+
+		this.render();
+
+	}
+	render(){
+
+		this.renderer.render(this.scene, this.camera);
 
 	}
 };
