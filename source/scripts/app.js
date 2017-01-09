6f8c9fd0d095240b1e397e93ca9daad8ecbbeeda
Jordan Delcros <contact@jordan-delcros.com>
2017-01-09 18:22:19 +0100

    Created vue + three boilerplate

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -6,6 +6,8 @@ Vue.use(Router);
 
 import HomeView from "./views/home-view.js";
 
+import * as THREE from "three";
+
 var router = new Router({
 	mode: "history",
 	base: "/",
@@ -21,15 +23,72 @@ var view = new Vue({
 	router: router,
 	created(){
 
-		console.info("Try to resize me to test the event bus.");
+		this.renderer = new THREE.WebGLRenderer({
+			precision: "highp",
+			antialias: true
+		});
 
-		window.addEventListener("resize", this.resize, false);
+		this.renderer.shadowMap.enabled = true;
+		this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+		this.scene = new THREE.Scene();
+
+		this.camera = new THREE.PerspectiveCamera(45, 1, 0.1, 1000);
+		this.camera.position.set(0, 0, 10);
+
+		window.addEventListener("resize", this.resize.bind(this), false);
+
+		this.resize();
+		this.start();
+
+		return this;
+
+	},
+	mounted(){
+
+		this.$el.appendChild(this.renderer.domElement);
 
 	},
 	methods: {
 		resize(){
 
+			this.width = window.innerWidth;
+			this.height = window.innerHeight;
+			this.ratio = (this.width / this.height);
+
+			this.camera.aspect = this.ratio;
+			this.camera.updateProjectionMatrix();
+
+			this.renderer.setSize(this.width, this.height);
+			this.renderer.setPixelRatio(window.devicePixelRatio);
+
 			EventHandler.$emit("resize");
+
+			return this;
+
+		},
+		start(){
+
+			this.animationFrame = window.requestAnimationFrame(this.update.bind(this));
+
+		},
+		stop(){
+
+			window.cancelAnimationFrame(this.animationFrame);
+
+		},
+		update( now ){
+
+			this.animationFrame = window.requestAnimationFrame(this.update.bind(this));
+
+			EventHandler.$emit("update");
+
+			this.render();
+
+		},
+		render(){
+
+			this.renderer.render(this.scene, this.camera);
 
 		}
 	}
--- source/scripts/views/home-view.js
+++ source/scripts/views/home-view.js
@@ -1,4 +1,7 @@
+import * as THREE from "three";
 import EventHandler from "../tools/event-handler.js";
+
+const DEGREE = (Math.PI / 180);
 
 export default {
 	name: "Home",
@@ -12,13 +15,41 @@ export default {
 	},
 	created(){
 
+		var texture = new THREE.TextureLoader().load("/assets/images/default.jpg");
+
+		this.cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1, 1), new THREE.MeshPhongMaterial({
+			map: texture
+		}));
+		this.cube.castShadow = true;
+		this.$root.scene.add(this.cube);
+
+		this.plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10, 1, 1), new THREE.MeshPhongMaterial());
+		this.plane.rotation.x = -(90 * DEGREE);
+		this.plane.position.set(0, -2, 0);
+		this.plane.receiveShadow = true;
+		this.$root.scene.add(this.plane);
+
+		this.light = new THREE.PointLight(0xFFFFFF, 1, 100);
+		this.light.position.set(0, 10, 10);
+		this.light.castShadow = true;
+		this.light.shadow.mapSize.width = this.light.shadow.mapSize.height = 2048;
+		this.$root.scene.add(this.light);
+
 		EventHandler.$on("resize", this.resize);
+		EventHandler.$on("update", this.update);
 
 	},
 	methods: {
 		resize(){
 
 			console.log("The $root told me to launch resize");
+
+		},
+		update(){
+
+			this.cube.rotation.x += (DEGREE * 1);
+			this.cube.rotation.y += (DEGREE * 1);
+			this.cube.rotation.z += (DEGREE * 1);
 
 		}
 	}
