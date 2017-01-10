c10429d173d61183b0c5f4642cd43b6f81f1a368
Jordan Delcros <contact@jordan-delcros.com>
2017-01-10 14:09:21 +0100

    Created node-mysql boilerplate

--- server.js
+++ server.js
@@ -1,14 +1,65 @@
 var http = require("http");
+var mysql = require("mysql");
+
+var database = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "azerty",
+	database: "boilerplate-io"
+});
+
+database.connect(function( error ){
+
+	if( error ){
+
+		throw error;
+
+	};
+
+	console.log("Database connected.");
+
+});
+
+database.query("CREATE TABLE IF NOT EXISTS `requests` (`id` BIGINT(20) NOT NULL AUTO_INCREMENT, `path` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8", ( error, rows, field )=>{
+
+	if( error ){
+
+		throw error;
+
+	};
+
+	if( !rows.warningCount ){
+
+		console.log("Created 'requests' table.");
+
+	};
+
+});
 
 var server = http.createServer(( request, results )=>{
 
-	console.log(new Date(), request.url);
+	var url = request.url;
 
-	results.writeHead(200, {
-		"Content-Type": "text/html"
+	database.query("INSERT INTO `requests` (`path`) VALUES (?)", url, function( error ){
+
+		results.writeHead(200, {
+			"Content-Type": "text/html"
+		});
+
+		if( error ){
+
+			console.error(error);
+
+			results.end("<p>Cannot interact with the database or the 'request' table.</p>");
+
+		}
+		else {
+
+			results.end("<p>Database table updated!</p>");
+
+		};
+
 	});
-
-	results.end("<p>Hello World!</p>");
 
 });
 
